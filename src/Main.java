6a3deaa4fb2e6939e1970732e4c2ac4bb63cac41
Ewgeny Leontev <leocross.ew@gmail.com>
2022-03-04 17:00:04 +0300

    First commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticketCost = 3000;
+        int mileCost = 20;
+        int milesTotalCount = ticketCost / mileCost;
+        System.out.println(milesTotalCount);
+    }
+}
